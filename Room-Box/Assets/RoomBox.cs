9a2dbc70ada32260cf7360e3f4c21febf4babfc3
Lukas Knöpfel <shylux@gmail.com>
2017-10-22 21:02:27 +0200

    Added empty Room-Box Project.

---
+++ Room-Box/Assets/RoomBox.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBox : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}

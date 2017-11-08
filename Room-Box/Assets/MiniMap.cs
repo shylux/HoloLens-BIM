2a7d70c10f89c4c74e07678842fac13a05b7d920
Lukas Knöpfel <shylux@gmail.com>
2017-11-08 18:02:12 +0100

    Working mini maxi voice commands.

--- Room-Box/Assets/MiniMap.cs
+++ Room-Box/Assets/MiniMap.cs
@@ -4,16 +4,47 @@ using UnityEngine;
 
 public class MiniMap : MonoBehaviour {
 
+    public enum State { MAXI_MAP, MINI_MAP }
+
+    public State state = State.MAXI_MAP;
+    private float transitionStartTime;
+
     public Material lineMaterial;
+    public float tableScaleFactor = 10;
+    public float transitionTime = 0.5f;
     
 	void Start () {
 
-        addCube(new Vector3(0, 0, 25), new Vector3(2, 3, 2));
+        addCube(new Vector3(-0.5f, 1, -0.8f), new Vector3(7, 3, 10));
 	}
-	
-	void Update () {
-		
-	}
+
+    void Update() {
+
+        float transitionProgress = (Time.realtimeSinceStartup - transitionStartTime) / transitionTime;
+        switch (state) {
+            case State.MAXI_MAP:
+                transform.localScale = Vector3.Lerp(Vector3.one / tableScaleFactor, Vector3.one, transitionProgress);
+                break;
+            case State.MINI_MAP:
+                transform.localScale = Vector3.Lerp(Vector3.one, Vector3.one / tableScaleFactor, transitionProgress);
+                break;
+            default:
+                Debug.Log("Something went reeealy wrong.");
+                break;
+        }
+    }
+
+    public void setState(string stateName) {
+        if (stateName == "MAXI_MAP" && state != State.MAXI_MAP) {
+            // transition to maxi map
+            state = State.MAXI_MAP;
+            transitionStartTime = Time.realtimeSinceStartup;
+        } else if (stateName == "MINI_MAP" && state != State.MINI_MAP) {
+            // transition to mini map
+            state = State.MINI_MAP;
+            transitionStartTime = Time.realtimeSinceStartup;
+        }
+    }
 
     public void addCube(Vector3[] corners) {
         LineRenderer rend = createRenderer();
@@ -49,7 +80,8 @@ public class MiniMap : MonoBehaviour {
     }
 
     // This is a helper intendet to simulate a room
-    public void addCube(Vector3 origin, Vector3 dimensions) {
+    public void addCube(Vector3 center, Vector3 dimensions) {
+        Vector3 origin = new Vector3(center.x - dimensions.x / 2, center.y - dimensions.y / 2, center.z - dimensions.z / 2);
         Vector3[] corners = new Vector3[8];
         corners[0] = origin;
         corners[1] = new Vector3(origin.x + dimensions.x, origin.y, origin.z);

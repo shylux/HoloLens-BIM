5a24f283f75f0fb34549bfe324c30a960713989a
Lukas Knöpfel <shylux@gmail.com>
2017-12-22 11:19:53 +0100

    Implements priority queue to determine baking order.

---
+++ Room-Box/Assets/Scripts/PriorityQueue.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+/**
+ * This is a priority queue where the priority is only evaluated when an item should be popped.
+ * Intended for usage where the priority changes often and reordering every time would be too time consuming.
+ * */
+public class LazyPriorityQueue<TValue> : List<TValue> {
+
+    private IComparer<TValue> comparer;
+
+    public LazyPriorityQueue(IComparer<TValue> _comparer) {
+        this.comparer = _comparer;
+    }
+
+    public TValue Pop() {
+        TValue topPrioValue = this[0];
+        foreach (TValue val in this) {
+            if (this.comparer.Compare(val, topPrioValue) < 0)
+                topPrioValue = val;
+        }
+        return topPrioValue;
+    }
+}
